0bba7d8c05ec038a0c5f4e62b06c0f15bcc11d93
Matthew Z <drmatthew1982@163.com>
2020-03-11 15:32:18 +0800

    update: show ip in service response

--- ribbonServer/src/main/java/exspringcloud/ribbon/controller/Controller.java
+++ ribbonServer/src/main/java/exspringcloud/ribbon/controller/Controller.java
@@ -4,20 +4,28 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 	private static Logger log = LoggerFactory.getLogger(Controller.class);
-
+	@Autowired
+    private HttpServletRequest request;
+	
 	@RequestMapping(value = "/greeting")
 	public String greet() {
 		log.info("Access /greeting");
 
-		List<String> greetings = Arrays.asList("Hi there", "Greetings", "Salutations");
+		String serverName = request.getServerName();
+		int serverPort = request.getServerPort();
+		String declaimation = "From:"+serverName+":"+serverPort+": ";
+		List<String> greetings = Arrays.asList(declaimation+"Hi there", declaimation+"Greetings", declaimation+"Salutations");
 		Random rand = new Random();
 
 		int randomNum = rand.nextInt(greetings.size());
--- serviceRegEureka/src/main/java/exspringcloud/ribbon/controller/Controller.java
+++ serviceRegEureka/src/main/java/exspringcloud/ribbon/controller/Controller.java
@@ -4,22 +4,30 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 	private static Logger log = LoggerFactory.getLogger(Controller.class);
+	@Autowired
+    private HttpServletRequest request;
+	
 
 	@RequestMapping(value = "/greeting")
 	public String greet() {
 		log.info("Access /greeting");
-
-		List<String> greetings = Arrays.asList("Hi there", "Greetings", "Salutations");
+		String serverName = request.getServerName();
+		int serverPort = request.getServerPort();
+		String declaimation = "From:"+serverName+":"+serverPort+": ";
+		List<String> greetings = Arrays.asList(declaimation+"Hi there", declaimation+"Greetings", declaimation+"Salutations");
 		Random rand = new Random();
-
 		int randomNum = rand.nextInt(greetings.size());
 		return greetings.get(randomNum);
 	}
